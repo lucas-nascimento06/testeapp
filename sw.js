3fd9053e2b5be486cd26be48538ace0d6cbd3d5a
Lucas Nascimento <laravelcrmsysttem@gmail.com>
2026-05-05 11:29:32 -0300

    nova alteracao

--- sw.js
+++ sw.js
@@ -1,3 +1,7 @@
 self.addEventListener("install", (e) => {
   console.log("Service Worker ativo");
 });
+
+self.addEventListener("notificationclick", function(event) {
+  event.notification.close();
+});
